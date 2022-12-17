115c7695e8cb5b3905f9cd20ceeedb53140b1abf
EvgenyWas <evgeniywas@gmail.com>
2022-12-17 13:08:19 +0000

    feature: added composables, services and http files to the project

---
+++ src/composables/index.ts
@@ -0,0 +1,5 @@
+import useStorage from './useStorage';
+import useUpload from './useUpload';
+import useDownload from './useDownload';
+
+export { useStorage, useUpload, useDownload };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,36 +1,3 @@
-import type { STORAGE_KEYS } from '@/config';
-import { ref } from 'vue';
+import FileSharingService from './FileSharingService';
 
-export default function useStorage<T>(
-  key: STORAGE_KEYS,
-  initialValue: T,
-  storage = localStorage
-) {
-  let storageValue = ref<any>();
-  try {
-    const value = storage.getItem(key) as string;
-    storageValue.value = JSON.parse(value);
-  } catch (error) {
-    storageValue.value = initialValue;
-  }
-
-  function setItem(item: T) {
-    try {
-      const value = JSON.stringify(item);
-      storageValue.value = item;
-      storage.setItem(key, value);
-    } catch (error) {
-      storageValue.value = initialValue;
-    }
-  }
-
-  function removeItem() {
-    try {
-      storage.removeItem(key);
-    } catch (error) {
-      storageValue.value = '';
-    }
-  }
-
-  return { storageValue, setItem, removeItem };
-}
+export const fileSharing = new FileSharingService();
