9d2dc7043dad4f4818fd8c3c5bbbf9ba73188523
EvgenyWas <evgeniywas@gmail.com>
2023-01-20 17:59:26 +0000

    feature: created file sharing service

--- src/services/FileSharingService.ts
+++ src/services/FileSharingService.ts
@@ -1,1 +1,25 @@
-export default class FileSharingService {}
+import http from '@/http-common';
+import type { TFormDataOptions } from '@/types';
+import { appendFormDataOptions, getSignData } from '@/utils';
+import type { AxiosRequestConfig } from 'axios';
+
+export default class FileSharingService {
+  private _api_key: string;
+  private _api_secret: string;
+
+  constructor(api_key: string, api_secret: string) {
+    (this._api_key = api_key), (this._api_secret = api_secret);
+  }
+
+  async uploadFileAuto(formData: FormData, config?: AxiosRequestConfig) {
+    const { timestamp, signature } = getSignData(this._api_secret);
+    const formDataOptions: TFormDataOptions = {
+      api_key: this._api_key,
+      timestamp: timestamp,
+      signature: signature,
+    };
+    const finishedFormData = appendFormDataOptions(formData, formDataOptions);
+
+    return http.post('/auto/uplaod', finishedFormData, config);
+  }
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,6 @@
 import FileSharingService from './FileSharingService';
 
-export const fileSharing = new FileSharingService();
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+const API_SECRET = import.meta.env.VITE_APP_API_SECRET;
+
+export const fileSharing = new FileSharingService(API_KEY, API_SECRET);
