1ac88a5c883e203d2272a7eb42cf35847b051b78
EvgenyWas <evgeniywas@gmail.com>
2023-01-20 17:04:46 +0000

    feature: added function util to get sign data with cloudinary api sign request

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,20 @@
 import type { TFormDataOptions } from '@/types';
+import { v2 as cloudinary } from 'cloudinary';
 
 export function getTimestamp(): number {
   return Math.round(new Date().getTime() / 1000);
+}
+
+export function getSignData(secret: string) {
+  const timestamp = getTimestamp();
+  const signApiOptions = {
+    timestamp: timestamp,
+    eager: 'c_pad,h_300,w_400|c_crop,h_200,w_260',
+    folder: 'signed_upload_demo_form',
+  };
+  const signature = cloudinary.utils.api_sign_request(signApiOptions, secret);
+
+  return { timestamp, signature };
 }
 
 export function appendFormDataOptions(
