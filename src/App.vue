c8c00d29776f89f17e832c436c5ed866001db03f
EvgenyWas <evgeniywas@gmail.com>
2023-01-24 22:44:24 +0000

    feature: added logic to create an user id and store it in local storage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <main class="main">
-    <MainBox />
+    <MainBox :userId="storageValue" />
   </main>
 </template>
 
 <script setup lang="ts">
+import { v4 as uuidv4 } from 'uuid';
+import { onMounted } from 'vue';
 import MainBox from './components/MainBox/MainBox.vue';
+import { useStorage } from './composables';
+import { STORAGE_KEYS } from './config';
+
+const { storageValue, setItem } = useStorage(STORAGE_KEYS.USER_ID, '');
+
+onMounted(() => {
+  if (!storageValue.value) {
+    setItem(uuidv4());
+  }
+});
 </script>
 
 <style lang="scss">
